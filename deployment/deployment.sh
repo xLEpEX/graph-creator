c7d1b38b183de869361674b25c0990ffb45adad5
leon phung <leon.phung@gmx.net>
2023-01-20 00:41:55 +0100

    add name to blank.yml

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -9,5 +9,5 @@ export IMAGE="docker.io/xlepex/graph-api"
 export DOMAIN="graph-api.phung.cc"
 export VERSION=$(git rev-parse --short HEAD)
 
-kubectl kustomize deployment/template | envsubst > deployment/prod.yml
+kubectl kustomize deployment/templates | envsubst > deployment/prod.yml
 
